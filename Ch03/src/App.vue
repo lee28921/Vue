e068375a596ec8ec866a7f5de8e352abe8b2c73e
dlwlals6790 <dlwlals6790@gmail.com>
2023-11-07 20:55:46 +0900

    컴포넌트

---
+++ Ch03/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import MyComponent1 from "./components/MyComponent1.vue";
+import MyComponent2 from "./components/MyComponent2.vue";
+import MyComponent3 from "./components/MyComponent3.vue";
+import OptionComponent1 from "./components/OptionComponent1.vue";
+import OptionComponent2 from "./components/OptionComponent2.vue";
+import OptionComponent3 from "./components/OptionComponent3.vue";
+</script>
+
+<template>
+  <h3>Ch03 컴포넌트</h3>
+  <h3>1) 컴포넌트</h3>
+  <MyComponent1 />
+  <MyComponent2 />
+  <MyComponent3 />
+
+  <h3>2) Options API</h3>
+  <OptionComponent1 />
+  <OptionComponent2 />
+  <OptionComponent3 />
+
+  <h3>3) 컴포넌트 통신</h3>
+  <h3>4) 컴포넌트 생명주기</h3>
+</template>
+
+<style scoped></style>
---
+++ Ch03/src/components/OptionComponent2.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name: "OptionComponent2",
+  methods: {
+    btn1Click() {
+      alert("button1 click!");
+    },
+    btn2Click: function () {
+      alert("button2 click!");
+    },
+    btn3Click: () => {
+      alert("button3 click!");
+    },
+  },
+};
+</script>
+<template>
+  <h4>methods 옵션</h4>
+  <button v-on:click="btn1Click">button1</button>
+  <button @:click="btn2Click">button2</button>
+  <button @:click="btn3Click">button3</button>
+</template>
+<style scoped></style>
