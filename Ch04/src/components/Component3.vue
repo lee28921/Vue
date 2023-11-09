ad65e7f969f13dd04952e619cfa9ac5b305af23e
dlwlals6790 <dlwlals6790@gmail.com>
2023-11-09 11:02:13 +0900

    Composition API

---
+++ Ch04/src/components/Component3.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Component4 from "./Component4.vue";
+import { provide } from "vue";
+
+const user2 = {
+  name: "홍길동",
+  age: 23,
+  addr: "부산",
+};
+
+provide("user2", user2);
+</script>
+<template>
+  <h4>Component3</h4>
+  <Component4 />
+</template>
+<style scoped></style>
